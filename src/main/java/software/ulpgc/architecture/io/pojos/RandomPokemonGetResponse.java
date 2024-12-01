95c04cc01f1e2620b5bb642b3da594f7c5c26a7b
Alberto José Rodríguez Ruano <alberto.rodriguez.2708@gmail.com>
2024-12-01 16:50:31 +0000

    Initial commit

---
+++ src/main/java/software/ulpgc/architecture/io/pojos/RandomPokemonGetResponse.java
@@ -0,0 +1,11 @@
+package software.ulpgc.architecture.io.pojos;
+
+import java.util.List;
+
+public record
+RandomPokemonGetResponse(String name, List<AbilityContainer> abilities, int height, int weight, Sprite sprites) {
+
+    public record AbilityContainer(Ability ability, boolean is_hidden) {}
+    public record Ability(String name) {}
+    public record Sprite(String front_default, String front_shiny) {}
+}
